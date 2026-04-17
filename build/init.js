c2cf235dd8e596143cea8e4b451d470c0334675e
Tai Nguyen <tai1nguyen@outlook.com>
2026-04-16 21:39:35 -0400

    feat: update init to clean and rebuild dist (#5)

--- build/init.js
+++ build/init.js
@@ -1,7 +1,9 @@
 const fs = require("node:fs");
 const { dist } = require("./config");
 
-// ensure dist exist
-if (!fs.existsSync(dist)) {
+if (fs.existsSync(dist)) {
+  fs.rmSync(dist, { recursive: true, force: true });
+  fs.mkdirSync(dist);
+} else {
   fs.mkdirSync(dist);
 }
